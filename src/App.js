caa8af8a4111a721e824c031cf462aea3c0b025c
RRaihan1234 <raihanspare123@gmail.com>
2024-06-27 12:24:38 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
-import DateTimePicker from 'react-datetime-picker'
+import uuid from 'react-uuid'
 import './App.css';
-import data from '../src/data.json'
 
 function App() {
   const [newTodo, setNewTodo] = useState([]);
@@ -10,12 +9,16 @@ function App() {
   const [showForm, setShowForm] = useState(false);
   const [showAddBtn, setShowAddtn] = useState(true);
   const [title, setTitle] = useState('');
-  const [desc, setDesc] = useState('');
-  const [rightClick, setRightClick] = useState(false);
-  const [render, setRender] = useState(false);
-  const [date,setDate] = useState();
-  const [expire, setExpire] = useState(false);
+  const [desc, setDesc] = useState('');;
+  const [, setRender] = useState(false);
   const [dateAndTime, setDateAndTime] = useState(new Date());
+    
+  useEffect(()=>{
+    let timer = setInterval(()=>{
+       setDateAndTime(new Date());
+    },1000)
+    return ()=>clearInterval(timer);
+  },[])
 
   const handleShowForm = () => {
         setShowForm(true)
@@ -36,23 +39,11 @@ function App() {
 
   const handleSubmit = (e) => {
         e.preventDefault();
-        let lengthMinusOne = newTodo.length - 1;
-        console.log(newTodo.length);
-        console.log(newTodo)
-        if(lengthMinusOne === -1){
-          newTodo.unshift({
+        newTodo.unshift({
             todoTitle : title,
             todoDesc : desc,
-            id : 1
+            id : uuid(),
           })
-        }else{
-          newTodo.unshift({
-            todoTitle : title,
-            todoDesc : desc,
-            id : newTodo[0].id + 1
-          })
-        }
-        console.log(newTodo);
         setShowForm(false);
         setShowAddtn(true);
   }
@@ -72,23 +63,15 @@ function App() {
   }
 
   const handleMoveToOngoingfromNew = (id) => {
-        let lengthMinusOne = onGoingTodo.length - 1;
         let movingToOngoingItem = newTodo.filter((item)=>{
           return item.id === id;
         })
-        if(lengthMinusOne === -1){
-          onGoingTodo.push({
+        onGoingTodo.push({
             todoTitle : movingToOngoingItem[0].todoTitle,
             todoDesc : movingToOngoingItem[0].todoDesc,
-            id : 1
+            id : uuid(),
           });
-        }else{
-          onGoingTodo.push({
-            todoTitle : movingToOngoingItem[0].todoTitle,
-            todoDesc : movingToOngoingItem[0].todoDesc,
-            id : onGoingTodo[lengthMinusOne].id + 1
-          });
-        }
+        
         setRender((prevState)=>!prevState)
 
         let remainingNewTodo = newTodo.filter((item)=>{
@@ -99,24 +82,14 @@ function App() {
   }
 
   const handleMoveToDonefromNew = (id) => {
-   
-    let lengthMinusOne = doneTodo.length - 1;
     let movingToDoneItem = newTodo.filter((item)=>{
       return item.id === id;
     })
-   if(lengthMinusOne === -1){
     doneTodo.push({
       todoTitle : movingToDoneItem[0].todoTitle,
       todoDesc : movingToDoneItem[0].todoDesc,
-      id : 1
+      id : uuid(),
     });
-   }else{
-    doneTodo.push({
-      todoTitle : movingToDoneItem[0].todoTitle,
-      todoDesc : movingToDoneItem[0].todoDesc,
-      id : doneTodo[lengthMinusOne].id + 1
-    });
-   }
     setRender((prevState)=>!prevState)
 
     let remainingNewTodo = newTodo.filter((item)=>{
@@ -141,23 +114,14 @@ function App() {
  }
 
   const handleMoveToNewfromOngoing = (id)=> {
-    let lengthMinusOne = newTodo.length - 1;
     let movingToNewItem = onGoingTodo.filter((item)=>{
       return item.id === id;
     })
-    if(lengthMinusOne === -1){
-      newTodo.push({
+    newTodo.push({
         todoTitle : movingToNewItem[0].todoTitle,
         todoDesc : movingToNewItem[0].todoDesc,
-        id : 1
+        id : uuid(),
       });
-    }else{
-      newTodo.push({
-        todoTitle : movingToNewItem[0].todoTitle,
-        todoDesc : movingToNewItem[0].todoDesc,
-        id : newTodo[lengthMinusOne].id + 1
-      });
-    }
     setRender((prevState)=>!prevState)
 
     let remainingOnGoingTodo = onGoingTodo.filter((item)=>{
@@ -167,27 +131,16 @@ function App() {
   }
 
   const handleMoveToDonefromOngoing = (id)=> {
-    let lengthMinusOne = doneTodo.length - 1;
     let movingToDoneItem = onGoingTodo.filter((item)=>{
       return item.id === id;
     })
-    
-    if(lengthMinusOne === -1){
-      doneTodo.push({
+    doneTodo.push({
         todoTitle : movingToDoneItem[0].todoTitle,
         todoDesc : movingToDoneItem[0].todoDesc,
-        id : 1
+        id : uuid(),
       });
-    }else{
-      doneTodo.push({
-        todoTitle : movingToDoneItem[0].todoTitle,
-        todoDesc : movingToDoneItem[0].todoDesc,
-        id : doneTodo[lengthMinusOne].id + 1
-      });
-    }
     setRender((prevState)=>!prevState)
     
-
     let remainingOnGoingTodo = onGoingTodo.filter((item)=>{
       return item.id!==id;
     })
@@ -209,27 +162,16 @@ function App() {
  }
 
   const handleMoveToNewfromDone = (id) => {
-
-    let lengthMinusOne = newTodo.length - 1;
     let movingToNewItem = doneTodo.filter((item)=>{
       return item.id === id;
     })
-    
-    if(lengthMinusOne === -1){
-      newTodo.push({
+    newTodo.push({
         todoTitle : movingToNewItem[0].todoTitle,
         todoDesc : movingToNewItem[0].todoDesc,
-        id : 1
+        id : uuid(),
       });
-    }else{
-      newTodo.push({
-        todoTitle : movingToNewItem[0].todoTitle,
-        todoDesc : movingToNewItem[0].todoDesc,
-        id : newTodo[lengthMinusOne].id + 1
-      });
-    }
+
     setRender((prevState)=>!prevState)
-    
 
     let remainingDoneTodo = doneTodo.filter((item)=>{
       return item.id!==id;
@@ -238,27 +180,18 @@ function App() {
 
   }
   const handleMoveToOnGoingfromDone = (id) => {
-    let lengthMinusOne = onGoingTodo.length - 1;
     let movingToOnGoingItem = doneTodo.filter((item)=>{
       return item.id === id;
     })
     
-    if(lengthMinusOne === -1){
-      onGoingTodo.push({
+    onGoingTodo.push({
         todoTitle : movingToOnGoingItem[0].todoTitle,
         todoDesc : movingToOnGoingItem[0].todoDesc,
-        id : 1
+        id : uuid(),
       });
-    }else{
-      onGoingTodo.push({
-        todoTitle : movingToOnGoingItem[0].todoTitle,
-        todoDesc : movingToOnGoingItem[0].todoDesc,
-        id : onGoingTodo[lengthMinusOne].id + 1
-      });
-    }
-    setRender((prevState)=>!prevState)
     
-
+    setRender((prevState)=>!prevState)
+  
     let remainingDoneTodo = doneTodo.filter((item)=>{
       return item.id!==id;
     })
@@ -266,37 +199,22 @@ function App() {
   }
 
   const handleDateChange = (e, id) => {
-    console.log(id)
-    setDate(e.target.value)
-    console.log(e.target.value)
+    setRender((prevState)=>!prevState);
+    const selectedTodo = onGoingTodo.filter((item)=>{
+      return item.id === id;
+    })
+    selectedTodo[0].dateSelected = e.target.value;
   }
-
-  useEffect(()=>{
-    /* let timer = setInterval(()=>{
-      setDateAndTime(new Date());
-    },1000)
-    console.log(timer)
-    console.log(dateAndTime) */
-    console.log(date);
-    console.log(new Date);
-    console.log(new Date(date))
-    if(new Date() > new Date(date)){
-      setExpire(true);
-    }else{
-      setExpire(false);
-    }
-  })
-  
 
   return (
     <div className="App">
        <div className="newParent">
-          <h3>New</h3>
+          <h2>New Todo</h2>
           {newTodo.map((item)=>{
             return(
-              <div className='newTodo' key={Math.random()} onContextMenu={(e)=> handleNewContextMenu(e, item.id)}>
-                  <h5>{item.todoTitle}</h5>
-                  <p>{item.todoDesc}</p>
+              <div className='newTodo' key={item.id} onContextMenu={(e)=> handleNewContextMenu(e, item.id)}>
+                  <h4>Title : {item.todoTitle}</h4>
+                  <p>Description : {item.todoDesc}</p>
                   {item.rightClick && <div className="movingOptionsParent">
                        <li className="movingOption" onClick={()=>handleMoveToOngoingfromNew(item.id)}>Move to Ongoing</li>
                        <li className="movingOption" onClick={()=>handleMoveToDonefromNew(item.id)}>Move to Done</li>
@@ -304,30 +222,34 @@ function App() {
              </div>
             )
           })}
-          {showAddBtn && <button onClick={handleShowForm}>Add a Todo</button>}
+          {showAddBtn && <button onClick={handleShowForm} className="addBtn">Add a Todo</button>}
           {showForm &&  <form onSubmit={handleSubmit}>
-              <div>
-                <label>Todo Title : </label>
-                <input type='text' required onChange={handleTitle}/>
+              <div className="formContent">
+                  <div>
+                    <label>Todo Title : </label>
+                    <input type='text' required onChange={handleTitle}/>
+                  </div>
+                  <div>
+                    <label>Todo Description : </label>
+                    <input type='text' required onChange={handleDesc}/>
+                  </div>
+                  <div className="cancelSaveContainer">
+                    <span onClick={handleCancel} className="cancelAdd">Cancel</span>
+                    <button type='submit' className="saveBtn">Save Todo</button>
+                  </div>
               </div>
-              <div>
-                <label>Todo Description : </label>
-                <input type='text' required onChange={handleDesc}/>
-              </div>
-              <span onClick={handleCancel}>Cancel</span>
-              <button type='submit'>Save Todo</button>
           </form>}
        </div>
 
        <div className='onGoingParent'>
-          <h3>Ongoing</h3>
+          <h2>Ongoing Todo</h2>
           {onGoingTodo.map((item)=>{
             return(
-              <div className='onGoingTodo' key={Math.random()} onContextMenu={(e)=> handleOngoingContextMenu(e, item.id)}>
-                  <h5>{item.todoTitle}</h5>
-                  <p>{item.todoDesc}</p>
-                  <p>End Time : <input type="datetime-local" onChange={(e)=>handleDateChange(e, item.id)} value={date}/></p>
-                  {expire && <p className="expireMsg">Overdue</p>}
+              <div className='onGoingTodo' key={item.id} onContextMenu={(e)=> handleOngoingContextMenu(e, item.id)}>
+                  <h4>Title : {item.todoTitle}</h4>
+                  <p>Description : {item.todoDesc}</p>
+                  <p>End Time : <input type="datetime-local" onChange={(e)=>handleDateChange(e, item.id)} value={item.dateSelected?item.dateSelected:''}/></p>
+                  {dateAndTime > new Date(item.dateSelected) && item.dateSelected && <p className="expireMsg">Overdue</p>}
                   {item.rightClick && <div className="movingOptionsParent">
                        <li className="movingOption" onClick={()=>handleMoveToNewfromOngoing(item.id)}>Move to New</li>
                        <li className="movingOption" onClick={()=>handleMoveToDonefromOngoing(item.id)}>Move to Done</li>
@@ -338,12 +260,12 @@ function App() {
        </div>
 
        <div className='doneParent'>
-          <h3>Done</h3>
+          <h2>Done Todo</h2>
           {doneTodo.map((item)=>{
             return(
-              <div className='doneTodo' key={Math.random()} onContextMenu={(e)=> handleDoneContextMenu(e, item.id)}>
-                  <h5>{item.todoTitle}</h5>
-                  <p>{item.todoDesc}</p>
+              <div className='doneTodo' key={item.id} onContextMenu={(e)=> handleDoneContextMenu(e, item.id)}>
+                  <h4>Title : {item.todoTitle}</h4>
+                  <p>Description : {item.todoDesc}</p>
                   {item.rightClick && <div className="movingOptionsParent">
                        <li className="movingOption" onClick={()=>handleMoveToNewfromDone(item.id)}>Move to New</li>
                        <li className="movingOption" onClick={()=>handleMoveToOnGoingfromDone(item.id)}>Move to Ongoing</li>
@@ -351,7 +273,7 @@ function App() {
              </div>
             )
           })}
-       </div>
+       </div>   
     </div>
   );
 }
